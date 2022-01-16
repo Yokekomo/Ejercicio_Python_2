0ff5563916d9e4ceaf1dff74a27b8f14002db986
Albano <77249799+Yokekomo@users.noreply.github.com>
2022-01-16 15:54:42 +0100

    Ejercicios Python

---
+++ Ejercicios.py
@@ -0,0 +1,38 @@
+
+"""    Ejercicio 1:
+• Crea un programa que pida dos números por teclado. El programa tendrá una función
+llamada “DevuelveMax” encargada de devolver el número más alto de los dos
+introducidos.
+"""
+
+"""    Ejercicio 2:
+• Crea un programa que pida por teclado “Nombre”, “Dirección” y “Tfno”. Esos tres datos 
+deberán ser almacenados en una lista y mostrar en consola el mensaje: “Los datos 
+personales son: nombre apellido teléfono” (Se mostrarán los datos introducidos por 
+teclado).
+"""
+
+"""    Ejercicio 3:
+• Crea un programa que pida tres números por teclado. El programa imprime en consola 
+la media aritmética de los números introducidos.
+"""
+
+"""    Ejercicio 4:
+• Crea un programa que muestre los números impares del 1 al 100. Los números deberán 
+aparecer una al lado del otro sin salto de línea.
+"""
+
+"""    Ejercicio 5:
+• Crea un programa que pida por teclado introducir una contraseña. La contraseña no 
+podrá tener menos de 8 caracteres ni espacios en blanco. Si la contraseña es correcta, 
+el programa imprime “Contraseña OK”. En caso contrario imprime “Contraseña 
+errónea"
+"""
+
+"""    Ejercicio 6:
+• Crea un programa que evalúe si una dirección de correo electrónico es válida o no en 
+función de si tiene “@” o “.” Hay que tener en cuenta que la dirección se considera 
+correcta si solo tiene una “@” y si tiene uno o más “."
+"""
+
+
---
+++ python04.py
@@ -0,0 +1,9 @@
+
+"""    Ejercicio 4:
+• Crea un programa que muestre los números impares del 1 al 100. Los números deberán
+aparecer una al lado del otro sin salto de línea.
+"""
+
+for i in range(1, 100):
+    if i % 2 == 1:
+        print(i, end=' ')
